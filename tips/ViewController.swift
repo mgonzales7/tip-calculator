85c2b622e709cef931fb65d0fadcce9c251a15c7
mgonzales7 <mgonzales7@fordham.edu>
2015-12-30 19:07:51 -0500

    Initial Commit, App Complete

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var outputView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        outputView.alpha = 0
+        tipControl.alpha = 0
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEdit(sender: AnyObject) {
+        
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        let tipP = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipP
+        let total = billAmount + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        if (billField.text != ""){
+            UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                self.tipControl.alpha = 1.0
+                self.outputView.alpha = 1.0
+                }, completion: nil)
+            
+        }
+        else {
+            UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {self.tipControl.alpha = 0.0
+                self.outputView.alpha = 0.0
+                }, completion: nil)
+            
+        }
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
